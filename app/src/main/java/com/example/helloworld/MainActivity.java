172d8b39121f9693d826d20e7b075484fc016a49
Cecheng Chen <cchen549@wisc.edu>
2021-09-15 19:49:13 -0500

    delete ImageView

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imgview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
